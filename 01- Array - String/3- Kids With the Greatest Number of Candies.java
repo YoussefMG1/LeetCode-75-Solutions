890b06fcd5d89e686e59325da9b3111373f13165
YoussefMG1 <113015622+YoussefMG1@users.noreply.github.com>
2025-10-24 02:07:04 +0300

    Implement solution for kids with greatest candies

---
+++ 01- Array - String/3- Kids With the Greatest Number of Candies.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+  
+  public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+    int max = candies[0];
+    for (int i = 1; i < candies.length; i++)
+      if(candies[i] > max) max = candies[i];
+
+    ArrayList<Boolean> result = new ArrayList<>();
+    for (int i = 0; i < candies.length; i++) {
+      if(candies[i] + extraCandies >= max)
+        result.add(true);
+      else result.add(false);
+    }
+    return result;
+  }
+
+}
