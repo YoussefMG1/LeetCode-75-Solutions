badac432c19021cded67d57a17d78e21d9b277cc
YoussefMG1 <113015622+YoussefMG1@users.noreply.github.com>
2025-10-24 02:08:48 +0300

    Implement canPlaceFlowers method

---
+++ 01- Array - String/4- Can Place Flowers.java
@@ -0,0 +1,22 @@
+public class Solution {
+  
+  public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+    int emptyAdjacentSlots = 0;
+    int maxSlots = 0;
+    boolean firstSlotsEmpty = true;
+    for (int i = 0; i < flowerbed.length; i++) {
+      if(flowerbed[i] == 0) emptyAdjacentSlots+=1;
+      else {
+        if(firstSlotsEmpty){
+          firstSlotsEmpty = false;
+          maxSlots += (emptyAdjacentSlots) /2;
+        }
+        else maxSlots += (emptyAdjacentSlots-1) / 2;
+        emptyAdjacentSlots = 0;
+      }
+    }
+    if(firstSlotsEmpty)   maxSlots += (emptyAdjacentSlots+1) /2;
+    else maxSlots += emptyAdjacentSlots/2;
+    return maxSlots >= n ;
+  }
+}
